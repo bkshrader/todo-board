91feda7a248760e94a4a6a0438959c1d4b1631a7
Bradley Shrader <bradleykshrader@gmail.com>
2025-06-23 19:12:02 -0400

    Add destroy button component

---
+++ resources/js/components/button-destroy.tsx
@@ -0,0 +1,34 @@
+import { Trash } from 'lucide-react';
+import { useState } from 'react';
+import { Button } from './ui/button';
+
+type DestroyButtonProps = React.ComponentProps<typeof Button>;
+// TODO add support for a confirm message
+export default function DestroyButton({ variant, onClick, onBlur, children, ...props }: DestroyButtonProps) {
+    const [showConfirm, setShowConfirm] = useState(false);
+    const handleClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+
+        // Set button to confirm state if it is not already
+        if (!showConfirm) {
+            setShowConfirm(true);
+            return;
+        }
+
+        onClick?.(e);
+    };
+
+    const handleBlur: React.FocusEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+
+        setShowConfirm(false);
+
+        handleBlur?.(e);
+    };
+
+    return (
+        <Button data-confirm={showConfirm} variant={showConfirm ? 'destructive' : variant} onClick={handleClick} onBlur={handleBlur} {...props}>
+            {children ?? <Trash />}
+        </Button>
+    );
+}
